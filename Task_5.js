8881c69cf03f26e82bae2df909acf3dd95455512
nkululek0 <68812334+nkululek0@users.noreply.github.com>
2020-08-12 11:28:52 +0200

    Update and rename Task_5.html to Task_5.js

---
+++ Task_5.js
@@ -0,0 +1,7 @@
+function triangleArea(side1, side2, side3){
+		let sum = (side1 + side2 + side3) * 0.5;
+		let area = Math.sqrt(sum * ((sum - side1) * (sum - side2) * (sum - side3)));
+		let roundedArea = Math.round(area);
+		console.log(roundedArea);
+	}
+	triangleArea(2,4,5);
