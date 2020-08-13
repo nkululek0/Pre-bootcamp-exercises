2c588202abd163c714c6a8fd2fc64a3fcecd8435
nkululek0 <68812334+nkululek0@users.noreply.github.com>
2020-08-13 17:07:47 +0200

    Update Task_8.js

--- Task_8.js
+++ Task_8.js
@@ -1,35 +1,44 @@
-	function number3(a, b){
-		let total = a + b;
-		let sum = String(total);//the total is turned into a string in order to be able to looped through it
-		let sumCounter = 0;
-		let argumentCounter = 0;
+function time(number, hour, minute){
+	let space = " ";
+	let hourString = "Hour";
+	let minString = "Minute";
+	let result;
+	let actualMinutes;
 
-			//this keeps count of whether the input contains a 3 or not
-				//if sumCounter is more than 1 that means one of the inputs contains a 3
-				//which increases the odds of the output being true
-			for(count = 0; count < sum.length; count++){
-				numberIndex = sum[count];
-					if(numberIndex == 3){
-						sumCounter += 1;
-					}
-					else{
-						sumCounter += 0;
-					}
+//calculation of hours
+	function hour(){
+		if(number < 60){
+			result = 0;
+		}
+		else if(number >= 60){
+				let inputHour = number / 60;
+					result = Math.round(inputHour);
+						if(result > 1){
+							hourString += "s";
+						}
+						else{
+							hourString;
+						}
 			}
+		}
 
-			//this checks to see if either of the variables/inputs are a 3
-			if(a == 3 || b == 3){
-				argumentCounter += 1;
-			}
-			else{
-				argumentCounter += 0;
-			}
-
-			//this checks to see if the sum of the inputs contains a 3;
-			if(sumCounter >= 1 && argumentCounter >= 1){
-				return true;
-			}
-			else{
-				return false;
+//calculation of minutes
+	function minute(){
+		if(number < 60){
+			actualMinutes = number;
+		}
+		else{
+			let inputMinutes = number / 60;
+			let rounded = Math.round(inputMinutes);
+			let leftOver = inputMinutes - rounded;
+			let minutes = leftOver * 60;
+				actualMinutes = Math.round(minutes);
+				if(actualMinutes > 1){
+					minString += "s";
+				}
+				else{minString}
 			}
 	}
+
+	console.log(result + space + hourString + "," + space + actualMinutes + space + minString);
+}
