a194d8f3d319c2739932b7ba3168dee01954fca3
nkululek0 <68812334+nkululek0@users.noreply.github.com>
2020-08-13 13:51:40 +0200

    Update Task_8.js

--- Task_8.js
+++ Task_8.js
@@ -1,22 +1,35 @@
-	function time(hours){
-		let Hour = "Hour";
-		let inputHour = hours / 60;
-		let result = Math.round(inputHour);
-			if(result > 1){
-				Hour += "s";
-			}
-			else{Hour};
+	function number3(a, b){
+		let total = a + b;
+		let sum = String(total);//the total is turned into a string in order to be able to looped through it
+		let sumCounter = 0;
+		let argumentCounter = 0;
 
-		let Min = "Minute";
-		let inputMinutes = hours / 60;
-		let rounded = Math.round(inputMinutes);
-		let leftOver = inputMinutes - rounded;
-		let minutes = leftOver * 60;
-		let actualMinutes = Math.round(minutes);
-			if(actualMinutes > 1){
-				Min += "s";
+			//this keeps count of whether the input contains a 3 or not
+				//if sumCounter is more than 1 that means one of the inputs contains a 3
+				//which increases the odds of the output being true
+			for(count = 0; count < sum.length; count++){
+				numberIndex = sum[count];
+					if(numberIndex == 3){
+						sumCounter += 1;
+					}
+					else{
+						sumCounter += 0;
+					}
 			}
-			else{Min}
-		
-		console.log(result + ": " + Hour + " " + actualMinutes + ":" + Min);	
+
+			//this checks to see if either of the variables/inputs are a 3
+			if(a == 3 || b == 3){
+				argumentCounter += 1;
+			}
+			else{
+				argumentCounter += 0;
+			}
+
+			//this checks to see if the sum of the inputs contains a 3;
+			if(sumCounter >= 1 && argumentCounter >= 1){
+				return true;
+			}
+			else{
+				return false;
+			}
 	}
