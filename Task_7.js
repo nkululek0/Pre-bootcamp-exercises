d1b1f222a72e2067eceea22acecc198f50f6375a
nkululek0 <68812334+nkululek0@users.noreply.github.com>
2020-08-18 16:52:18 +0200

    Task_7

--- Task_7.js
+++ Task_7.js
@@ -1,11 +1,11 @@
-	function farenheitConvertor(temperature){
-		let farenheit = (temperature * 9/5) + 32;
-		let celsiusPlaceHolder = "deg Celsius";
-			console.log(farenheit + " " + celsiusPlaceHolder);
-	}
-	function celsiusConvertor(temperature){
-		let celsius = (temperature - 32)/9*5;
-		let rounded = Math.round(celsius);
-		let farenheitPlaceHolder = "deg Farenheit";
-			console.log(rounded + " " + farenheitPlaceHolder);
-	}
+function farenheitConvertor(temperature){
+	let farenheit = (temperature * 9/5) + 32;
+	let celsiusPlaceHolder = "deg Celsius";
+	return (farenheit + " " + celsiusPlaceHolder);
+}
+function celsiusConvertor(temperature){
+	let celsius = (temperature - 32)/9*5;
+	let rounded = Math.round(celsius);
+	let farenheitPlaceHolder = "deg Farenheit";
+	return (rounded + " " + farenheitPlaceHolder);
+}
