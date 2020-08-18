0c3c575f1d0f978d4e3ef54da50fa40d171c2e96
nkululek0 <68812334+nkululek0@users.noreply.github.com>
2020-08-18 16:59:14 +0200

    Task_11

--- Task_11.js
+++ Task_11.js
@@ -1,13 +1,14 @@
-	function commonLetters(word1, word2){
-		let sameLetter = [];
-			for(count1 = 0; count1 < word1.length; count1++){
-				for(count2 = 0; count2 < word2.length; count2++){
-					let letters1 = word1[count1];
-					let letters2 = word2[count2];
-						if(letters1 == letters2){
-							sameLetter.push(letters1);
-						}
-				}		
-			}
-			console.log("Common Letters: " + sameLetter);
-	}
+function commonLetters(word1, word2) {
+    let sameLetter = [];
+    for (count1 = 0; count1 < word1.length; count1++) {
+        for (count2 = 0; count2 < word2.length; count2++) {
+            let letters1 = word1[count1];
+            let letters2 = word2[count2];
+            if (letters1 == letters2) {
+                sameLetter.push(letters1);
+            }
+        }
+    }
+    console.log(sameLetter);
+    console.log('Common Letters: ' + sameLetter);
+}
