d0836ed92ccc037d309fbd03ad8c8e42dc0bb53b
nkululek0 <68812334+nkululek0@users.noreply.github.com>
2020-08-18 16:28:32 +0200

    Task_1

--- Task_1.js
+++ Task_1.js
@@ -1,11 +1,11 @@
-	x = 0;
-	y = 1;
+x = 0;
+y = 1;
 
-	console.log(x);
-	console.log(y);
+console.log(x);
+console.log(y);
 
-	x += 3;
-	y += x;
+x += 3;
+y += x;
 
-	console.log(x);
-	console.log(y);
+console.log(x);
+console.log(y);
