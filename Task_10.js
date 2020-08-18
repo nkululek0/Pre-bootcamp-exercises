8d436f88eff65dec011053c0c72bfeb7bc0f6209
nkululek0 <68812334+nkululek0@users.noreply.github.com>
2020-08-18 16:57:08 +0200

    Task_10

--- Task_10.js
+++ Task_10.js
@@ -1,12 +1,12 @@
-	function vowelReturner(word){
-		let vowelDefiner = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
-			for(count = 0; count <= word.length; count++){
-				for(count2 = 0; count2 <= vowelDefiner.length; count2++){
-					let vowel = vowelDefiner[count2];
-					let letter = word[count];
-						if(letter == vowel){
-							console.log(letter);
-						}
-				}		
+function vowelReturner(word){
+	let vowelDefiner = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
+	for (count = 0; count <= word.length; count++){
+			for (count2 = 0; count2 <= vowelDefiner.length; count2++){
+			let vowel = vowelDefiner[count2];
+			let letter = word[count];
+			if (letter == vowel){
+				console.log(letter);
 			}
-	}	
+			}		
+		}
+}
