ee9fd1d9dab2af31c0d3f5bebaa866eca6478ee5
nkululek0 <68812334+nkululek0@users.noreply.github.com>
2020-08-18 16:35:00 +0200

    Task_4

--- Task_4.js
+++ Task_4.js
@@ -1,19 +1,18 @@
-	function number3(a, b){
-		let total = a + b;
-		let sum = String(total);
-
-			if(a == 3 || b == 3){
-				for(count = 0; count < sum.length; count++){
-					let numberIndex = sum[count];
-						if(numberIndex == 3){
-							return true;
-						}
-						else{
-							return false;
-						}
-				}
-			}
-			else{
-				return false;
-			}
-	}
+function number3(a, b) {
+    let total = a + b;
+    let sum = String(total);
+    if (a == 3 || b == 3) {
+        for (count = 0; count < sum.length; count++) {
+            let numberIndex = sum[count];
+            if (numberIndex == 3) {
+                return true;
+            } 
+            else {
+                return false;
+            }
+        }
+    } 
+    else {
+        return false;
+    }
+}
