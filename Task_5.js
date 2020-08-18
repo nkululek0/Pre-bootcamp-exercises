70d96a7c8072474e92fecd4d20d7a38bb99c49ef
nkululek0 <68812334+nkululek0@users.noreply.github.com>
2020-08-18 15:14:11 +0200

    Task_5

--- Task_5.js
+++ Task_5.js
@@ -2,5 +2,5 @@ function triangleArea(side1, side2, side3){
 		let sum = (side1 + side2 + side3) * 0.5;
 		let area = Math.sqrt(sum * ((sum - side1) * (sum - side2) * (sum - side3)));
 		let roundedArea = Math.round(area);
-		console.log(roundedArea);
+		return roundedArea;
 	}
