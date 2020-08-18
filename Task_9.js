a90377f8b4783ae2a453a8f0d30958f74d43e3d4
nkululek0 <68812334+nkululek0@users.noreply.github.com>
2020-08-18 16:55:36 +0200

    Task_9

--- Task_9.js
+++ Task_9.js
@@ -1,19 +1,13 @@
-	function multiples(number){
-		let sum = 0;
-		let totalMultiples = [];
-			for(count = (number - 1); count > 0; count--){
-				if(count % 3 == 0){
-					totalMultiples.push(count);
-				}
-				else if(count % 5 == 0){
-					totalMultiples.push(count);
-				}
-
-			}
-
-			for(count = 0; count < totalMultiples.length; count++){
-				sum += totalMultiples[count];
-			}
-			console.log(sum);
-			
-	}
+function multiples(){
+	let sum = 0;
+	let totalMultiples = [];
+	for (count = 999; count > 0; count--) {
+        if ((count % 3 == 0) || (count % 5 == 0)) {
+            totalMultiples.push(count);
+        }
+    }
+    for (count = 0; count < totalMultiples.length; count++) {
+        sum += totalMultiples[count];
+    }
+    console.log(sum);
+}
